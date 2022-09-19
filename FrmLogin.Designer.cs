ee954ea1d30d261ba558a09d94792368952c17bf
HanenJMS <Hanensgamelab@gmail.com>
2022-09-19 17:27:03 -0400

    intial Commit

---
+++ FrmLogin.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PersonalTracking
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserNo = new System.Windows.Forms.Label();
+            this.TxtUserNo = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.Label();
+            this.ButtonEnter = new System.Windows.Forms.Button();
+            this.ButtonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UserNo
+            // 
+            this.UserNo.AutoSize = true;
+            this.UserNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNo.Location = new System.Drawing.Point(50, 38);
+            this.UserNo.Name = "UserNo";
+            this.UserNo.Size = new System.Drawing.Size(84, 25);
+            this.UserNo.TabIndex = 0;
+            this.UserNo.Text = "UserNo";
+            // 
+            // TxtUserNo
+            // 
+            this.TxtUserNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtUserNo.Location = new System.Drawing.Point(149, 33);
+            this.TxtUserNo.Name = "TxtUserNo";
+            this.TxtUserNo.Size = new System.Drawing.Size(209, 30);
+            this.TxtUserNo.TabIndex = 0;
+            this.TxtUserNo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtUserNo_KeyPress);
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtPassword.Location = new System.Drawing.Point(149, 80);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(209, 30);
+            this.TxtPassword.TabIndex = 1;
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(28, 80);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(106, 25);
+            this.Password.TabIndex = 0;
+            this.Password.Text = "Password";
+            // 
+            // ButtonEnter
+            // 
+            this.ButtonEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonEnter.Location = new System.Drawing.Point(24, 151);
+            this.ButtonEnter.Name = "ButtonEnter";
+            this.ButtonEnter.Size = new System.Drawing.Size(158, 53);
+            this.ButtonEnter.TabIndex = 2;
+            this.ButtonEnter.Text = "Enter";
+            this.ButtonEnter.UseVisualStyleBackColor = true;
+            // 
+            // ButtonExit
+            // 
+            this.ButtonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonExit.Location = new System.Drawing.Point(197, 151);
+            this.ButtonExit.Name = "ButtonExit";
+            this.ButtonExit.Size = new System.Drawing.Size(158, 53);
+            this.ButtonExit.TabIndex = 3;
+            this.ButtonExit.Text = "Exit";
+            this.ButtonExit.UseVisualStyleBackColor = true;
+            this.ButtonExit.Click += new System.EventHandler(this.ButtonExit_Click);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 256);
+            this.Controls.Add(this.ButtonExit);
+            this.Controls.Add(this.ButtonEnter);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.TxtUserNo);
+            this.Controls.Add(this.UserNo);
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UserNo;
+        private System.Windows.Forms.TextBox TxtUserNo;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Button ButtonEnter;
+        private System.Windows.Forms.Button ButtonExit;
+    }
+}
+
