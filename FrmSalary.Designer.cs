624790397738aff7f8d61eebd0abc73ae4a409e9
HanenJMS <Hanensgamelab@gmail.com>
2022-09-24 18:44:56 -0400

    9/24 adding Salary Form

---
+++ FrmSalary.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace PersonalTracking
+{
+    partial class FrmSalary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.CmbPosition = new System.Windows.Forms.ComboBox();
+            this.TextPosition = new System.Windows.Forms.Label();
+            this.CmbDepartment = new System.Windows.Forms.ComboBox();
+            this.TextDepartment = new System.Windows.Forms.Label();
+            this.ButtonClose = new System.Windows.Forms.Button();
+            this.ButtonSave = new System.Windows.Forms.Button();
+            this.TextBoxSalary = new System.Windows.Forms.TextBox();
+            this.LabelSalary = new System.Windows.Forms.Label();
+            this.StateTask = new System.Windows.Forms.ComboBox();
+            this.LabelMonth = new System.Windows.Forms.Label();
+            this.TextBoxSurName = new System.Windows.Forms.TextBox();
+            this.TxtSurName = new System.Windows.Forms.Label();
+            this.TextBoxName = new System.Windows.Forms.TextBox();
+            this.TxtName = new System.Windows.Forms.Label();
+            this.TxtBoxUserNo = new System.Windows.Forms.TextBox();
+            this.UserNo = new System.Windows.Forms.Label();
+            this.TextBoxYear = new System.Windows.Forms.TextBox();
+            this.LabelYear = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(425, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(348, 585);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 198);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(348, 387);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.CmbPosition);
+            this.panel2.Controls.Add(this.TextPosition);
+            this.panel2.Controls.Add(this.CmbDepartment);
+            this.panel2.Controls.Add(this.TextDepartment);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(348, 198);
+            this.panel2.TabIndex = 3;
+            // 
+            // CmbPosition
+            // 
+            this.CmbPosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmbPosition.FormattingEnabled = true;
+            this.CmbPosition.Location = new System.Drawing.Point(39, 122);
+            this.CmbPosition.Name = "CmbPosition";
+            this.CmbPosition.Size = new System.Drawing.Size(184, 33);
+            this.CmbPosition.TabIndex = 1;
+            // 
+            // TextPosition
+            // 
+            this.TextPosition.AutoSize = true;
+            this.TextPosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextPosition.Location = new System.Drawing.Point(34, 94);
+            this.TextPosition.Name = "TextPosition";
+            this.TextPosition.Size = new System.Drawing.Size(89, 25);
+            this.TextPosition.TabIndex = 14;
+            this.TextPosition.Text = "Position";
+            // 
+            // CmbDepartment
+            // 
+            this.CmbDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmbDepartment.FormattingEnabled = true;
+            this.CmbDepartment.Location = new System.Drawing.Point(39, 47);
+            this.CmbDepartment.Name = "CmbDepartment";
+            this.CmbDepartment.Size = new System.Drawing.Size(184, 33);
+            this.CmbDepartment.TabIndex = 0;
+            // 
+            // TextDepartment
+            // 
+            this.TextDepartment.AutoSize = true;
+            this.TextDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextDepartment.Location = new System.Drawing.Point(34, 19);
+            this.TextDepartment.Name = "TextDepartment";
+            this.TextDepartment.Size = new System.Drawing.Size(123, 25);
+            this.TextDepartment.TabIndex = 13;
+            this.TextDepartment.Text = "Department";
+            // 
+            // ButtonClose
+            // 
+            this.ButtonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonClose.Location = new System.Drawing.Point(239, 414);
+            this.ButtonClose.Name = "ButtonClose";
+            this.ButtonClose.Size = new System.Drawing.Size(125, 54);
+            this.ButtonClose.TabIndex = 4;
+            this.ButtonClose.Text = "Close";
+            this.ButtonClose.UseVisualStyleBackColor = true;
+            this.ButtonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // ButtonSave
+            // 
+            this.ButtonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonSave.Location = new System.Drawing.Point(75, 414);
+            this.ButtonSave.Name = "ButtonSave";
+            this.ButtonSave.Size = new System.Drawing.Size(137, 54);
+            this.ButtonSave.TabIndex = 3;
+            this.ButtonSave.Text = "Save";
+            this.ButtonSave.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxSalary
+            // 
+            this.TextBoxSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxSalary.Location = new System.Drawing.Point(120, 223);
+            this.TextBoxSalary.Name = "TextBoxSalary";
+            this.TextBoxSalary.Size = new System.Drawing.Size(214, 30);
+            this.TextBoxSalary.TabIndex = 0;
+            this.TextBoxSalary.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxSalary_KeyPress);
+            // 
+            // LabelSalary
+            // 
+            this.LabelSalary.AutoSize = true;
+            this.LabelSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelSalary.Location = new System.Drawing.Point(26, 223);
+            this.LabelSalary.Name = "LabelSalary";
+            this.LabelSalary.Size = new System.Drawing.Size(74, 25);
+            this.LabelSalary.TabIndex = 43;
+            this.LabelSalary.Text = "Salary";
+            // 
+            // StateTask
+            // 
+            this.StateTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StateTask.FormattingEnabled = true;
+            this.StateTask.Location = new System.Drawing.Point(120, 320);
+            this.StateTask.Name = "StateTask";
+            this.StateTask.Size = new System.Drawing.Size(214, 33);
+            this.StateTask.TabIndex = 2;
+            // 
+            // LabelMonth
+            // 
+            this.LabelMonth.AutoSize = true;
+            this.LabelMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelMonth.Location = new System.Drawing.Point(29, 320);
+            this.LabelMonth.Name = "LabelMonth";
+            this.LabelMonth.Size = new System.Drawing.Size(72, 25);
+            this.LabelMonth.TabIndex = 42;
+            this.LabelMonth.Text = "Month";
+            // 
+            // TextBoxSurName
+            // 
+            this.TextBoxSurName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TextBoxSurName.Location = new System.Drawing.Point(156, 102);
+            this.TextBoxSurName.Name = "TextBoxSurName";
+            this.TextBoxSurName.ReadOnly = true;
+            this.TextBoxSurName.Size = new System.Drawing.Size(214, 30);
+            this.TextBoxSurName.TabIndex = 38;
+            // 
+            // TxtSurName
+            // 
+            this.TxtSurName.AutoSize = true;
+            this.TxtSurName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtSurName.Location = new System.Drawing.Point(12, 107);
+            this.TxtSurName.Name = "TxtSurName";
+            this.TxtSurName.Size = new System.Drawing.Size(102, 25);
+            this.TxtSurName.TabIndex = 41;
+            this.TxtSurName.Text = "SurName";
+            // 
+            // TextBoxName
+            // 
+            this.TextBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TextBoxName.Location = new System.Drawing.Point(156, 58);
+            this.TextBoxName.Name = "TextBoxName";
+            this.TextBoxName.ReadOnly = true;
+            this.TextBoxName.Size = new System.Drawing.Size(214, 30);
+            this.TextBoxName.TabIndex = 37;
+            // 
+            // TxtName
+            // 
+            this.TxtName.AutoSize = true;
+            this.TxtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtName.Location = new System.Drawing.Point(12, 63);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(68, 25);
+            this.TxtName.TabIndex = 40;
+            this.TxtName.Text = "Name";
+            // 
+            // TxtBoxUserNo
+            // 
+            this.TxtBoxUserNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtBoxUserNo.Location = new System.Drawing.Point(156, 14);
+            this.TxtBoxUserNo.Name = "TxtBoxUserNo";
+            this.TxtBoxUserNo.ReadOnly = true;
+            this.TxtBoxUserNo.Size = new System.Drawing.Size(214, 30);
+            this.TxtBoxUserNo.TabIndex = 36;
+            // 
+            // UserNo
+            // 
+            this.UserNo.AutoSize = true;
+            this.UserNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserNo.Location = new System.Drawing.Point(12, 19);
+            this.UserNo.Name = "UserNo";
+            this.UserNo.Size = new System.Drawing.Size(84, 25);
+            this.UserNo.TabIndex = 39;
+            this.UserNo.Text = "UserNo";
+            // 
+            // TextBoxYear
+            // 
+            this.TextBoxYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxYear.Location = new System.Drawing.Point(120, 269);
+            this.TextBoxYear.Name = "TextBoxYear";
+            this.TextBoxYear.Size = new System.Drawing.Size(214, 30);
+            this.TextBoxYear.TabIndex = 1;
+            // 
+            // LabelYear
+            // 
+            this.LabelYear.AutoSize = true;
+            this.LabelYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelYear.Location = new System.Drawing.Point(26, 269);
+            this.LabelYear.Name = "LabelYear";
+            this.LabelYear.Size = new System.Drawing.Size(57, 25);
+            this.LabelYear.TabIndex = 45;
+            this.LabelYear.Text = "Year";
+            // 
+            // FrmSalary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 585);
+            this.Controls.Add(this.TextBoxYear);
+            this.Controls.Add(this.LabelYear);
+            this.Controls.Add(this.ButtonClose);
+            this.Controls.Add(this.ButtonSave);
+            this.Controls.Add(this.TextBoxSalary);
+            this.Controls.Add(this.LabelSalary);
+            this.Controls.Add(this.StateTask);
+            this.Controls.Add(this.LabelMonth);
+            this.Controls.Add(this.TextBoxSurName);
+            this.Controls.Add(this.TxtSurName);
+            this.Controls.Add(this.TextBoxName);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.TxtBoxUserNo);
+            this.Controls.Add(this.UserNo);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmSalary";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Salary";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox CmbPosition;
+        private System.Windows.Forms.Label TextPosition;
+        private System.Windows.Forms.ComboBox CmbDepartment;
+        private System.Windows.Forms.Label TextDepartment;
+        private System.Windows.Forms.Button ButtonClose;
+        private System.Windows.Forms.Button ButtonSave;
+        private System.Windows.Forms.TextBox TextBoxSalary;
+        private System.Windows.Forms.Label LabelSalary;
+        private System.Windows.Forms.ComboBox StateTask;
+        private System.Windows.Forms.Label LabelMonth;
+        private System.Windows.Forms.TextBox TextBoxSurName;
+        private System.Windows.Forms.Label TxtSurName;
+        private System.Windows.Forms.TextBox TextBoxName;
+        private System.Windows.Forms.Label TxtName;
+        private System.Windows.Forms.TextBox TxtBoxUserNo;
+        private System.Windows.Forms.Label UserNo;
+        private System.Windows.Forms.TextBox TextBoxYear;
+        private System.Windows.Forms.Label LabelYear;
+    }
+}
